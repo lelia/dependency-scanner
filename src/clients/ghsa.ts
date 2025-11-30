2d7079c565b588767c4b370fce9760ed73ce2fc3
lelia <le1ia@me.com>
2025-11-30 17:48:44 -0500

    Stub API client for GHSA
    Signed-off-by: lelia <le1ia@me.com>

---
+++ src/clients/ghsa.ts
@@ -0,0 +1,8 @@
+/**
+ * API Client for GHSA (GitHub Security Advisory) database.
+ * 
+ * Optional: Fine-grained PAT (Personal Access Token) with security advisories scope.
+ * Usage: Export as `GITHUB_TOKEN` environment variable or pass as `--github-token` flag.
+ * 
+ * Ref: https://docs.github.com/en/rest/security-advisories/global-advisories
+ */
