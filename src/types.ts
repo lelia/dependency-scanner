3ffeef4363b0bfebb33b491c073fab9badc363e6
lelia <le1ia@me.com>
2025-11-30 02:20:53 -0500

    Introduce ecosystem to interface
    Signed-off-by: lelia <le1ia@me.com>

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,13 @@
  */
 
 export type DependencyType = "direct" | "transitive";
+export type Ecosystem = "npm" | "pypi";
 
 export interface DependencyNode {
   id: string;
   name: string;
   version: string;
+  ecosystem: Ecosystem;
   dependencyType: DependencyType;
   dependencies: string[];
 }
